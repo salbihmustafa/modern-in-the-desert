2c9995cd70f3bf6ef84d838a7aa11e0078dc7f0b
Salbih Mustafa <salbihdmustafa@gmail.com>
2022-06-06 14:02:48 -0500

    removed header title

--- src/App.js
+++ src/App.js
@@ -1,52 +1,52 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { ThemeProvider } from "react-bootstrap";
+import logo from "./image/ModernInTheDesertLogo.jpg"
 
 const App = () => {
   return (
-    <ThemeProvider
-      breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
-    >
-      <div className="main-container">
-        <header>
-          <div className="header-title">MODERN IN THE DESERT</div>
-        </header>
-        <div className="content">
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
-          <div className="center">Test</div>
+    <div className="main-container">
+      <header>
+        <div className="logo">
+          <img src={logo} alt="image" />
         </div>
-        <footer>
-          <div className="footer-title">© 2022 Modern in the Desert, All rights reserved.</div>
-        </footer>
+      </header>
+      <div className="content">
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
+        <div className="center">Test</div>
       </div>
-    </ThemeProvider>
+      <footer>
+        <div className="footer-title">
+          © 2022 Modern in the Desert, All rights reserved.
+        </div>
+      </footer>
+    </div>
   );
 };
 

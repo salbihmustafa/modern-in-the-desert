8707d1761199496f0130401f34f383f37826b25b
Salbih Mustafa <salbihdmustafa@gmail.com>
2022-06-05 21:48:20 -0500

    added footer and header with css media quieries

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { ThemeProvider } from "react-bootstrap";
 
 const App = () => {
-    return (
-        <div className="main-container">
-            <div className="content">
-                <div className="center">Test</div>
-            </div>
+  return (
+    <ThemeProvider
+      breakpoints={["xxxl", "xxl", "xl", "lg", "md", "sm", "xs", "xxs"]}
+    >
+      <div className="main-container">
+        <header>
+          <div className="header-title">MODERN IN THE DESERT</div>
+        </header>
+        <div className="content">
+          <div className="center">Test</div>
         </div>
-    )
-}
+        <footer>
+          <div className="footer-title">© 2022 Modern in the Desert, All rights reserved.</div>
+        </footer>
+      </div>
+    </ThemeProvider>
+  );
+};
 
 export default App;

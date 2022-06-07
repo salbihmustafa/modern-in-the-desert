a773dc3af67b481597a5a77838998239c111614d
Salbih Mustafa <salbihdmustafa@gmail.com>
2022-06-06 20:27:30 -0500

    found a work around with pdf.worker.js workaround

--- src/App.js
+++ src/App.js
@@ -1,45 +1,34 @@
 import "./App.css";
+import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import logo from "./image/ModernInTheDesertLogo.jpg"
+import logo from "./image/ModernInTheDesertLogo.jpg";
+import welcomebook from "./pdf/WelcomeBook.pdf";
+//import { Document, Page } from "react-pdf/dist/esm/entry.webpack";
+import { Document, Page } from "react-pdf";
 
 const App = () => {
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
+
+  const onDocumentLoadSuccess = ({ numPages }) => {
+    setNumPages(numPages);
+  };
+
   return (
     <div className="main-container">
       <header>
         <div className="logo">
-          <img src={logo} alt="image" />
+          <img src={logo} alt="Modern in the Desert Logo" />
         </div>
       </header>
       <div className="content">
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
-        <div className="center">Test</div>
+        <Document
+          file={welcomebook}
+          options={{ workerSrc: "pdf.worker.js" }}
+          onLoadSuccess={onDocumentLoadSuccess}
+        >
+          <Page pageNumber={1} />
+        </Document>
       </div>
       <footer>
         <div className="footer-title">
---
+++ src/components/DisplayPdf/DisplayPdf.js
@@ -0,0 +1,2 @@
+import { useState } from "react";
+import { Document, Page } from "react-pdf";
